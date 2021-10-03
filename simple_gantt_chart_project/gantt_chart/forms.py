f8e484e40ac01411cd08304af7dae6c5e1d0531d
Maciej Such <msuch92@gmail.com>
2021-10-03 21:07:19 +0200

    data json input hidden in css

--- simple_gantt_chart_project/gantt_chart/forms.py
+++ simple_gantt_chart_project/gantt_chart/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class jsonStringForm(forms.Form):
-    data = forms.CharField(max_length=50000)
+    data = forms.CharField(max_length=50000, label='', required=False)
     
 
 
